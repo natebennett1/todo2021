26e6a6f52ae9b20f67d174ba10e07e548a827d4a
natebennett1 <59584909+natebennett1@users.noreply.github.com>
2021-03-09 08:21:53 -0700

    todo finished

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDWrhWhomdIEkraT5YU3_cwMNrw_jOkIX8",
+  authDomain: "helloworld2021-ba411.firebaseapp.com",
+  databaseURL: "https://helloworld2021-ba411-default-rtdb.firebaseio.com",
+  projectId: "helloworld2021-ba411",
+  storageBucket: "helloworld2021-ba411.appspot.com",
+  messagingSenderId: "864565047128",
+  appId: "1:864565047128:web:dba6befd737b6e1b253f2d",
+  measurementId: "G-49ERS2R75J"
 };
 
 firebase.initializeApp(config);
